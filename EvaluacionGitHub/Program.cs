011957e8b1f419079fd16dfcc3fbf41d594e891e
DELL@LAPTOPDAVID <perezsamueldavid09@gmail.com>
2025-05-13 14:46:10 -0600

    Primer commit del proyecto

--- EvaluacionGitHub/Program.cs
+++ EvaluacionGitHub/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Proyecto de ventas");
+            Console.WriteLine("Proyecto de venta");
         }
     }
 }
